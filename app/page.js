5fd9093d3a81e8deced2df450c0505846878916c
gatien074 <ngouessygatien@gmail.com>
2024-11-05 18:52:27 +0100

    one3

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ import Button from "./components/Button";
 export default function Home() {
   const [isClient, setIsClient] = useState(false);
 
+
   useEffect(() => {
     setIsClient(true);
   }, []);
@@ -56,14 +57,14 @@ export default function Home() {
     <h2 className="text-3xl text-white py-10 lg:py-20 lg:text-4xl font-extrabold">Mes compétences</h2>
   </div>
 
-  <motion.div className="grid grid-cols-3 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 justify-items-center py-10 sm:py-12">
+  <div className="grid grid-cols-3 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 justify-items-center py-10 sm:py-12">
     <Image  className="w-16 h-10 sm:w-[50px] sm:h-12 lg:w-[50px] lg:h-[50px]  shadow-md shadow-white rounded-full" src="/images/html-removebg-preview.png" alt="HTML" width={1200} height={100} />
     <Image className="w-16 h-10 sm:w-[50px] sm:h-12 lg:w-[50px] lg:h-[50px] shadow-md shadow-white rounded-full" src="/images/css-removebg-preview.png" alt="CSS" width={1200} height={100} />
     <Image className="w-16 h-10 sm:w-[50px] sm:h-12 lg:w-[50px] lg:h-[50px] shadow-md shadow-white rounded-full" src="/images/nextjs-removebg-preview.png" alt="Next.js" width={1200} height={100} />
     <Image className="w-16 h-10 sm:w-[50px] sm:h-12 lg:w-[50px] lg:h-[50px] shadow-md shadow-white rounded-full" src="/images/react-removebg-preview.png" alt="React" width={1200} height={100} />
     <Image className="w-20 h-10 sm:w-[58px] sm:h-12 lg:w-[80px] lg:h-[50px] shadow-md  shadow-white  rounded-full" src="/images/images-removebg-preview.png" alt="Images" width={1200} height={100} />
     <Image className="w-16 h-10 sm:w-[50px] sm:h-12 lg:w-[50px] lg:h-[50px] shadow-md shadow-white rounded-full" src="/images/javascript-1.svg" alt="JavaScript" width={1200} height={100} />
-  </motion.div>
+  </div>
 </section>
       <footer className='mt-48'>
         <div className='flex justify-center '>
